8c76252b8ebe94c8150de352938fbbba5cba35cf
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-04-28 22:48:30 +1000

    Initial commit (default projects)

---
+++ flightpub-api/src/main/java/seng3150/team4/flightpub/FlightpubApplication.java
@@ -0,0 +1,13 @@
+package seng3150.team4.flightpub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightpubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightpubApplication.class, args);
+    }
+
+}
---
+++ flightpub-api/src/test/java/seng3150/team4/flightpub/FlightpubApplicationTests.java
@@ -0,0 +1,13 @@
+package seng3150.team4.flightpub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightpubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
