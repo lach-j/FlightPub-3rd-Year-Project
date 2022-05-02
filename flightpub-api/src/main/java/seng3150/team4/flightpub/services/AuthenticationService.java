a2b2b958cc62137cc15b03fbeda6f30c47ed7413
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-05-02 21:58:39 +1000

    SENG-4 - Add password hashing when checking login details

--- flightpub-api/src/main/java/seng3150/team4/flightpub/services/AuthenticationService.java
+++ flightpub-api/src/main/java/seng3150/team4/flightpub/services/AuthenticationService.java
@@ -2,6 +2,11 @@ package seng3150.team4.flightpub.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import seng3150.team4.flightpub.utility.PasswordHash;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -11,9 +16,13 @@ public class AuthenticationService implements IAuthenticationService {
 
     public String loginUser(String email, String password) {
         var user = userService.getUserByEmail(email);
-
+        try {
+            if (Objects.equals(user.getPassword(), PasswordHash.PBKDF2WithHmacSHA1Hash(password, email)))
+                return "REPLACE THIS WITH TOKEN GENERATION";
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            ex.printStackTrace();
+        }
         return null;
-        //TODO: IMPLEMENT PASSWORD HASHING AND JWT GENERATION
     }
 
 }
---
+++ flightpub-api/src/main/java/seng3150/team4/flightpub/utility/PasswordHash.java
@@ -0,0 +1,29 @@
+package seng3150.team4.flightpub.utility;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+public class PasswordHash {
+    public static String PBKDF2WithHmacSHA1Hash(String password, String salt) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(StandardCharsets.UTF_8), 65536, 512);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+        return bytesToHex(hash);
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
