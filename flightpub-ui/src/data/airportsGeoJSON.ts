3fa1273d6e01c5574914d3a636d95e7ae1f151d8
Lachlan Johnson <c3350131@uon.edu.au>
2022-05-12 16:47:38 +1000

    SENG-17 - Convert to GeoJSON and add conditional styling

---
+++ flightpub-ui/src/data/airportsGeoJSON.ts
@@ -0,0 +1,499 @@
+export const airportsGeoJSON = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.428067,
+          33.636719
+        ]
+      },
+      "properties": {
+        "name": "Hartsfield Jackson Atlanta Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.904842,
+          41.978603
+        ]
+      },
+      "properties": {
+        "name": "Chicago Ohare Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.461389,
+          51.4775
+        ]
+      },
+      "properties": {
+        "name": "Heathrow"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          2.55,
+          49.012779
+        ]
+      },
+      "properties": {
+        "name": "Charles De Gaulle"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -118.408075,
+          33.942536
+        ]
+      },
+      "properties": {
+        "name": "Los Angeles Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.778925,
+          40.639751
+        ]
+      },
+      "properties": {
+        "name": "John F Kennedy Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          4.763889,
+          52.308613
+        ]
+      },
+      "properties": {
+        "name": "Schiphol"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          103.994433,
+          1.350189
+        ]
+      },
+      "properties": {
+        "name": "Changi Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -80.290556,
+          25.79325
+        ]
+      },
+      "properties": {
+        "name": "Miami Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          11.786086,
+          48.353783
+        ]
+      },
+      "properties": {
+        "name": "Franz Josef Strauss"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          113.914603,
+          22.308919
+        ]
+      },
+      "properties": {
+        "name": "Hong Kong Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          55.364444,
+          25.252778
+        ]
+      },
+      "properties": {
+        "name": "Dubai Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.190278,
+          51.148056
+        ]
+      },
+      "properties": {
+        "name": "Gatwick"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.250797,
+          41.804475
+        ]
+      },
+      "properties": {
+        "name": "Fiumicino"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -3.566764,
+          40.493556
+        ]
+      },
+      "properties": {
+        "name": "Barajas"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          100.747283,
+          13.681108
+        ]
+      },
+      "properties": {
+        "name": "Suvarnabhumi Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -79.630556,
+          43.677223
+        ]
+      },
+      "properties": {
+        "name": "Lester B Pearson Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.569722,
+          48.110278
+        ]
+      },
+      "properties": {
+        "name": "Schwechat"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.386389,
+          35.764722
+        ]
+      },
+      "properties": {
+        "name": "Narita Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          101.709917,
+          2.745578
+        ]
+      },
+      "properties": {
+        "name": "Kuala Lumpur Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.374889,
+          37.618972
+        ]
+      },
+      "properties": {
+        "name": "San Francisco Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          151.177222,
+          -33.946111
+        ]
+      },
+      "properties": {
+        "name": "Sydney Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          2.359444,
+          48.725278
+        ]
+      },
+      "properties": {
+        "name": "Orly"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.565056,
+          25.261125
+        ]
+      },
+      "properties": {
+        "name": "Doha Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          28.246,
+          -26.139166
+        ]
+      },
+      "properties": {
+        "name": "Johannesburg Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.963333,
+          60.317222
+        ]
+      },
+      "properties": {
+        "name": "Helsinki Vantaa"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.872608,
+          40.777245
+        ]
+      },
+      "properties": {
+        "name": "La Guardia"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          153.1175,
+          -27.384167
+        ]
+      },
+      "properties": {
+        "name": "Brisbane Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.843333,
+          -37.673333
+        ]
+      },
+      "properties": {
+        "name": "Melbourne Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -157.922428,
+          21.318681
+        ]
+      },
+      "properties": {
+        "name": "Honolulu Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -43.243647,
+          -22.808903
+        ]
+      },
+      "properties": {
+        "name": "Galeao Antonio Carlos Jobim"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          115.966944,
+          -31.940278
+        ]
+      },
+      "properties": {
+        "name": "Perth Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          145.755278,
+          -16.885833
+        ]
+      },
+      "properties": {
+        "name": "Cairns Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          138.530556,
+          -34.945
+        ]
+      },
+      "properties": {
+        "name": "Adelaide Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          130.87266,
+          -12.4083333
+        ]
+      },
+      "properties": {
+        "name": "Darwin Intl"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          153.504722,
+          -28.164444
+        ]
+      },
+      "properties": {
+        "name": "Gold Coast"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          149.195,
+          -35.306944
+        ]
+      },
+      "properties": {
+        "name": "Canberra"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          147.510278,
+          -42.836111
+        ]
+      },
+      "properties": {
+        "name": "Hobart"
+      }
+    }
+  ]
+}
--- flightpub-ui/src/pages/MapPage.tsx
+++ flightpub-ui/src/pages/MapPage.tsx
@@ -2,34 +2,38 @@ import {
   Box,
   Icon,
 } from '@chakra-ui/react';
-import Map, { FullscreenControl, Marker } from 'react-map-gl';
+import Map, { FullscreenControl, Marker, GeolocateControl, Source, Layer, LayerProps } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { MdLocalAirport } from 'react-icons/all';
+import { airportsGeoJSON } from '../data/airportsGeoJSON';
+import { useState } from 'react';
 
 const airports = require('../data/airports.json');
 
 export const MapPage = () => {
+  const [selectedAirport, setSelectedAirport] = useState<object | undefined>();
   return (
     <Box h={'100vh'}>
       <Map
         initialViewState={{
           longitude: -122.4,
           latitude: 37.8,
-          zoom: 16,
+          zoom: 1,
         }}
         style={{ width: '100%', height: '100%' }}
         mapStyle='mapbox://styles/mapbox/streets-v11'
         mapboxAccessToken={'pk.eyJ1IjoiYzMzNTAxMzEiLCJhIjoiY2wwZXp1YzJoMG82MjNkcXQ5YmxsbWRtMCJ9.hoJ4MvSxn7j0J89DVLWaQw'}
       >
-        {airports.map((airport: any) => {
+        {
+          airportsGeoJSON.features.map((feature) => {
             return (
-              <Marker longitude={airport._geoloc.lng} latitude={airport._geoloc.lat}>
-                <Icon as={MdLocalAirport} boxSize={10} />
-              </Marker>);
-          },
-        )
+              <Marker longitude={feature.geometry.coordinates[0]} latitude={feature.geometry.coordinates[1]}>
+                <Icon cursor={'pointer'} as={MdLocalAirport} color={selectedAirport === feature ? 'red' : 'black'} fontSize={'3em'} onClick={() => {setSelectedAirport(feature)}}/>
+              </Marker>
+            );
+          })
         }
-        <FullscreenControl />
+        <GeolocateControl />
       </Map>
     </Box>
   );
