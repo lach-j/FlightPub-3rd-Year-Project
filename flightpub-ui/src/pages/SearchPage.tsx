41bd4664acf3cf7c1f99b45727a686f304ff137e
ChinniganCode <c3331609@uon.edu.au>
2022-05-13 20:56:36 +1000

    Improved search page, temporarily added hooks to test functionality and print values on the bottom of the page.
    TODO: Pass data to searchResults page and ensure form validation.

--- flightpub-ui/src/pages/SearchPage.tsx
+++ flightpub-ui/src/pages/SearchPage.tsx
@@ -1,14 +1,7 @@
 import {
     Box,
-    Flex,
     Text,
-    IconButton,
     Button,
-    Stack,
-    Collapse,
-    Icon,
-    Link,
-    Input,
     NumberInput,
     NumberInputField,
     NumberInputStepper,
@@ -21,127 +14,113 @@ import {
     FormLabel,
     Select,
     Center,
-    Avatar,
 } from '@chakra-ui/react';
 import React, {useState, SyntheticEvent} from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import {User} from "../models/User";
 import { CUIAutoComplete } from 'chakra-ui-autocomplete'
+import {
+    AutoComplete,
+    AutoCompleteInput,
+    AutoCompleteItem,
+    AutoCompleteList,
+} from "@choc-ui/chakra-autocomplete";
+import moment from "moment";
 
 export interface Item {
     label: string;
     value: string;
 }
 
-const tags = [{
-    value: "adelaide", label: "Adelaide"
-}, {value: "amsterdam", label: "Amsterdam"}, {
-    value: "atlanta", label: "Atlanta"
-}, {
-    value: "bangkok", label: "Bangkok"
-}, {
-    value: "brisbane", label: "Brisbane"
-}, {
-    value: "canberra", label: "Canberra"
-}, {
-    value: "paris - charles de gaulle", label: "Paris - Charles De Gaulle"
-}, {
-    value: "cairns", label: "Cairns"
-}, {
-    value: "doha", label: "Doha"
-}, {
-    value: "darwin", label: "Darwin"
-}, {
-    value: "dubai", label: "Dubai"
-}, {
-    value: "rome-fiumicino", label: "Rome-Fiumicino"
-}, {
-    value: "rio de janeiro", label: "Rio De Janeiro"
-}, {
-    value: "hobart", label: "Hobart"
-}, {
-    value: "helsinki", label: "Helsinki"
-}, {
-    value: "hong kong", label: "Hong Kong"
-}, {
-    value: "honolulu", label: "Honolulu"
-}, {
-    value: "new york - jfk", label: "New York - JFK"
-}, {
-    value: "johannesburg", label: "Johannesburg"
-}, {
-    value: "kuala lumpur", label: "Kuala Lumpur"
-}, {
-    value: "los angeles", label: "Los Angeles"
-}, {
-    value: "new york - laguardia", label: "New York - Laguardia"
-}, {
-    value: "london-gatwick", label: "London-Gatwick"
-}, {
-    value: "london-heathrow", label: "London-Heathrow"
-}, {
-    value: "madrid", label: "Madrid"
-}, {
-    value: "melbourne", label: "Melbourne"
-}, {
-    value: "miami", label: "Miami"
-}, {
-    value: "munich", label: "Munich"
-}, {
-    value: "tokyo - narita", label: "Tokyo - Narita"
-}, {
-    value: "gold coast", label: "Gold Coast"
-}, {
-    value: "chicago - ohare intl.", label: "Chicago - OHare Intl."
-}, {
-    value: "paris - orly", label: "Paris - Orly"
-}, {
-    value: "perth", label: "Perth"
-}, {
-    value: "san francisco", label: "San Francisco"
-}, {
-    value: "singapore", label: "Singapore"
-}, {
-    value: "sydney", label: "Sydney"
-}, {
-    value: "vienna", label: "Vienna"
-}, {
-    value: "toronto", label: "Toronto"
-}]
+const tags2 = [
+    {label: "Beach" , value: "beach"},
+    {label: "Snow" , value: "snow"},
+    {label: "Holiday" , value: "holiday"},
+    {label: "Family-Friendly", value: "family-friendly"},
+    {label: "Sports", value: "sports"},
+    {label: "Romantic", value: "romantic"},
+    {label: "Asia", value: "asia"},
+]
+
+const tags =
+    [
+    {value: "adelaide", label: "Adelaide"},
+    {value: "amsterdam", label: "Amsterdam"},
+    {value: "atlanta", label: "Atlanta"},
+    {value: "bangkok", label: "Bangkok"},
+    {value: "brisbane", label: "Brisbane"},
+    {value: "canberra", label: "Canberra"},
+    {value: "paris - charles de gaulle", label: "Paris - Charles De Gaulle"},
+    {value: "cairns", label: "Cairns"},
+    {value: "doha", label: "Doha"},
+    {value: "darwin", label: "Darwin"},
+    {value: "dubai", label: "Dubai"},
+    {value: "rome-fiumicino", label: "Rome-Fiumicino"},
+    {value: "rio de janeiro", label: "Rio De Janeiro"},
+    {value: "hobart", label: "Hobart"},
+    {value: "helsinki", label: "Helsinki"},
+    {value: "hong kong", label: "Hong Kong"},
+    {value: "honolulu", label: "Honolulu"},
+    {value: "new york - jfk", label: "New York - JFK"},
+    {value: "johannesburg", label: "Johannesburg"},
+    {value: "kuala lumpur", label: "Kuala Lumpur"},
+    {value: "los angeles", label: "Los Angeles"},
+    {value: "new york - laguardia", label: "New York - Laguardia"},
+    {value: "london-gatwick", label: "London-Gatwick"},
+    {value: "london-heathrow", label: "London-Heathrow"},
+    {value: "madrid", label: "Madrid"},
+    {value: "melbourne", label: "Melbourne"},
+    {value: "miami", label: "Miami"},
+    {value: "munich", label: "Munich"},
+    {value: "tokyo - narita", label: "Tokyo - Narita"},
+    {value: "gold coast", label: "Gold Coast"},
+    {value: "chicago - ohare intl.", label: "Chicago - OHare Intl."},
+    {value: "paris - orly", label: "Paris - Orly"},
+    {value: "perth", label: "Perth"},
+    {value: "san francisco", label: "San Francisco"},
+    {value: "singapore", label: "Singapore"},
+    {value: "sydney", label: "Sydney"},
+    {value: "vienna", label: "Vienna"},
+    {value: "toronto", label: "Toronto"}
+    ]
 
 const handleSearch = (e: SyntheticEvent) => {
 
 }
 
 export const SearchPage = () => {
-    const [loading, setLoading] = useState(false);
+    {/*TODO: Change these hooks to a single 'Search' model */}
+
+    const [flightClass, setFlightClass] = useState("");
+    const handleClassChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setFlightClass(event.target.value)
+    }
+
+    const [flightType, setFlightType] = useState("");
+    const handleTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setFlightType(event.target.value)
+    }
+    const [numPassengers, setNumPassengers] = useState("");
+
     const [departureDate, setDepartureDate] = useState(new Date());
     const [arrivalDate, setArrivalDate] = useState(new Date());
 
+    const [arrivalLocation, setArrivalLocation] = useState("");
+    const [departureLocation, setDepartureLocation] = useState("");
+    const [selectedTags, setSelectedTags] = React.useState<Item[]>([]);
 
-    const [pickerItems, setPickerItems] = React.useState(tags);
-    const [selectedItems, setSelectedItems] = React.useState<Item[]>([]);
 
-    const handleCreateItem = (item: Item) => {
-        setPickerItems((curr) => [...curr, item]);
-        setSelectedItems((curr) => [...curr, item]);
-    };
-
-    const handleSelectedItemsChange = (selectedItems?: Item[]) => {
-        if (selectedItems) {
-            setSelectedItems(selectedItems);
+    const handleSelectedItemsChange = (selectedTags?: Item[]) => {
+        if (selectedTags) {
+            setSelectedTags(selectedTags);
         }
     };
 
-    const customRender = <T extends Item>(selected: T) => {
-        return (
-            <Flex flexDir="row" alignItems="center">
-                <Avatar mr={2} size="sm" name={selected.label} />
-                <Text>{selected.label}</Text>
-            </Flex>
-        )
-    }
+
+
+
+
     return (
         <Grid>
             <Center>
@@ -154,7 +133,7 @@ export const SearchPage = () => {
                         align='stretch'>
                         <Box>
                             <FormLabel htmlFor='flightClass'>Class: </FormLabel>
-                            <Select name='flightClass' id='flightClass' placeholder='Select flight class' maxW={240}>
+                            <Select onChange={handleClassChange} name='flightClass' id='flightClass' placeholder='Select flight class' maxW={240}>
                                 <option>Economy</option>
                                 <option>Business</option>
                                 <option>First-class</option>
@@ -162,8 +141,8 @@ export const SearchPage = () => {
                         </Box>
 
                         <Box>
-                            <FormLabel htmlFor='flightType'>Class: </FormLabel>
-                            <Select name='flightType' id='flightType' placeholder='Select flight Type' maxW={240}>
+                            <FormLabel htmlFor='flightType'>Type: </FormLabel>
+                            <Select onChange={handleTypeChange} name='flightType' id='flightType' placeholder='Select flight Type' maxW={240}>
                                 <option>One-way</option>
                                 <option>Round trip</option>
                                 <option>Multi-city</option>
@@ -175,8 +154,10 @@ export const SearchPage = () => {
                             <NumberInput allowMouseWheel={true}
                                          name={"passengers"}
                                          size='sm'
+                                         min={1}
                                          maxW={16}
-                                         defaultValue={0}>
+                                         onChange={setNumPassengers}
+                                         defaultValue={1}>
                                 <NumberInputField />
                                 <NumberInputStepper>
                                     <NumberIncrementStepper />
@@ -191,45 +172,106 @@ export const SearchPage = () => {
                                     rounded: 'full'
                                 }}
                                 label="Select preferred tags"
-                                placeholder="Type a Country"
+                                placeholder="Select a tag"
                                 disableCreateItem={true}
-                                onCreateItem={handleCreateItem}
-                                items={pickerItems}
-                                selectedItems={selectedItems}
+                                items={tags2}
+                                selectedItems={selectedTags}
                                 onSelectedItemsChange={(changes) =>
                                     handleSelectedItemsChange(changes.selectedItems)
                                 }
                             />
                         </Box>
+
                         <Box>
                             <FormLabel>Arrival Location:</FormLabel>
-                            <Input name="arrival" variant='outline' placeholder='Arrival Location' width='auto' />
+                            <AutoComplete openOnFocus onChange={setArrivalLocation}>
+                                <AutoCompleteInput variant="filled" />
+                                <AutoCompleteList >
+                                    {tags.map((tag, cNum) => (
+                                        <AutoCompleteItem
+                                            key={`option-${cNum}`}
+                                            value={tag.label}
+                                            align="center"
+                                        >
+                                            <Text ml="4">{tag.label}</Text>
+                                        </AutoCompleteItem>
+                                    ))}
+                                </AutoCompleteList>
+                            </AutoComplete>
                         </Box>
 
                         <Box>
                             <FormLabel>Departure Location</FormLabel>
-                            <Input name="departure" variant='outline' placeholder='Departure Location' width='auto' />
+                            <AutoComplete openOnFocus onChange={setDepartureLocation}>
+                                <AutoCompleteInput variant="filled" />
+                                <AutoCompleteList>
+                                    {tags.map((tag, cNum) => (
+                                        <AutoCompleteItem
+                                            key={`option-${cNum}`}
+                                            value={tag.label}
+                                            align="center"
+                                        >
+                                            <Text ml="4">{tag.label}</Text>
+                                        </AutoCompleteItem>
+                                    ))}
+                                </AutoCompleteList>
+                            </AutoComplete>
                         </Box>
 
-                        <FormLabel>Arrival Date</FormLabel>
-                        <DatePicker
-                            minDate={new Date()}
-                            selected={arrivalDate}
-                            onChange={(date:Date) => setArrivalDate(date)} />
                         <FormLabel>Departure Date</FormLabel>
                         <DatePicker
                             minDate={new Date()}
                             selected={departureDate}
-                            onChange={(date:Date) => setDepartureDate(date)} />
+                            onChange={(date:Date) => setDepartureDate(date)}
+                        />
                         <Box>
-                            <Button type="submit" isLoading={loading} colorScheme="red">
+
+                        <FormLabel>Arrival Date</FormLabel>
+                        <DatePicker
+                            minDate={new Date(departureDate)}
+                            selected={arrivalDate}
+                            onChange={(date:Date) => setArrivalDate(date)} />
+
+
+                            <Button type="submit" colorScheme="red">
                                 Search
                             </Button>
                         </Box>
+
                     </VStack>
                 </FormControl>
             </form>
             </Center>
+
+            <VStack
+                divider={<StackDivider borderColor='gray.200' />}
+                spacing={2}
+                align='stretch'>
+                <Box>
+                    <p>Flight Class: {flightClass}</p>
+                </Box>
+                <Box>
+                    <p>Flight Type: {flightType}</p>
+                </Box>
+                <Box>
+                    <p>Number of Passengers: {numPassengers}</p>
+                </Box>
+                <Box>
+                    <ul>
+                        Tags: {selectedTags.map(itemtest => (
+                        <li key={itemtest.label}>{itemtest.value}</li>
+                    ))}
+                    </ul>
+                </Box>
+                <Box>
+                    <p>Arrival Location: {arrivalLocation}</p>
+                    <p>Departure Location: {departureLocation}</p>
+                </Box>
+                <Box>
+                    <p>Arrival Date: {moment(arrivalDate).format("LL")} </p>
+                    <p>Departure: {moment(departureDate).format("LL")}</p>
+                </Box>
+            </VStack>
         </Grid>
 
 
