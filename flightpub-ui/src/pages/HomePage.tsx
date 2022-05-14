557f80c96b0b5ca94b9d9ae7aaa0cf066994084e
deadeyesurf <kurtm60@gmail.com>
2022-05-14 10:15:58 +1000

    very quick and dirty implementation of the homepage is done

--- flightpub-ui/src/pages/HomePage.tsx
+++ flightpub-ui/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Center, Grid} from "@chakra-ui/react";
+import {Box, Button, Center, Grid, VStack} from "@chakra-ui/react";
 import logo from '../logo.png';
 
 console.log(logo);
@@ -9,7 +9,23 @@ export const HomePage = () => {
     return (
         <Grid>
             <Center>
-                <img src={logo} alt="Logo" />;
+                <VStack
+                    spacing={2}
+                    align='center'>
+                    <Box>
+                        <img src={logo} alt="Logo" width={'20%'}/>
+                    </Box>
+                    <Box>
+                        <form>
+                            <Button type="submit" colorScheme="red">
+                                Search For a Flight
+                            </Button>
+                            <Button type="submit" colorScheme="red">
+                                I'm Feeling Lucky
+                            </Button>
+                        </form>
+                    </Box>
+                </VStack>
             </Center>
         </Grid>
     )
