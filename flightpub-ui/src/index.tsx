1b1c8717c437d98fe73314efc626af364ab14a75
Lachlan Johnson <62972498+lach-j@users.noreply.github.com>
2022-05-15 15:15:46 +1000

    SENG-17 implement map page (#7)
    * SENG-17 Implement basic map wil airport layer
    
    * SENG-17 - Convert to GeoJSON and add conditional styling
    
    * Update gitignores
    
    * SENG-17 - Add ID to airport data
    
    * SENG-17 - Downgrade to react 17
    
    * SENG-17 - Add more data to dummy airports
    
    * SENG-17 - Add flight popup for destinations
    
    * SENG-17 - cleanup
    
    * SENG-17 - Geolocate on load

--- flightpub-ui/src/index.tsx
+++ flightpub-ui/src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
@@ -17,6 +14,7 @@ root.render(
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
+  document.getElementById('root') as HTMLElement
 );
 
 // If you want to start measuring performance in your app, pass a function
