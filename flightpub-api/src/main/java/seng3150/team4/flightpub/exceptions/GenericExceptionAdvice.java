68d8f872005916d374fab736c7be165774d0e88c
Lachlan Johnson <c3350131@uon.edu.au>
2022-05-20 16:04:46 +1000

    SENG-23 - add exception handling for unsupported operation

---
+++ flightpub-api/src/main/java/seng3150/team4/flightpub/exceptions/GenericExceptionAdvice.java
@@ -0,0 +1,20 @@
+package seng3150.team4.flightpub.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import seng3150.team4.flightpub.controllers.responses.Response;
+import seng3150.team4.flightpub.controllers.responses.StatusResponse;
+
+
+@ControllerAdvice
+public class GenericExceptionAdvice {
+    @ResponseBody
+    @ExceptionHandler(UnsupportedOperationException.class)
+    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
+    Response entityAlreadyExistsHandler(UnsupportedOperationException ex) {
+        return new StatusResponse(HttpStatus.NOT_IMPLEMENTED, ex.getMessage());
+    }
+}
