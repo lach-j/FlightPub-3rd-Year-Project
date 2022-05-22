24a55ec3b58cc055d94109c93de4b2c4b9080d52
Lachlan Johnson <c3350131@uon.edu.au>
2022-05-22 15:04:41 +1000

    SENG-24 Clean up UI and add backend integration

--- flightpub-ui/src/pages/SearchResultsPage.tsx
+++ flightpub-ui/src/pages/SearchResultsPage.tsx
@@ -3,15 +3,7 @@ import {
     Box,
     Button,
     Center,
-    FormControl,
-    FormErrorMessage,
-    FormLabel,
-    Grid,
-    Heading,
-    Input,
-    Link,
     Select,
-    Stack,
     useToast,
     Thead,
     Table,
@@ -27,9 +19,6 @@ import {
     SliderTrack,
     SliderFilledTrack,
     SliderThumb,
-    SliderMark,
-    Flex,
-    Spacer,
     HStack,
     VStack,
     StackDivider,
@@ -37,18 +26,27 @@ import {
 import {
     TriangleDownIcon, TriangleUpIcon,
 } from '@chakra-ui/icons';
-import { Link as RouteLink, useNavigate } from 'react-router-dom';
-import { routes } from '../constants/routes';
-import React, { SyntheticEvent, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import * as _ from 'lodash';
 
-const columns = [
-    {accessor: 'AirlineName', Header: 'Airline'},
-    {accessor: 'DepartureAirport', Header: 'Departure Airport'},
-    {accessor: 'DepartureTime', Header: 'Departure Time'},
-    {accessor: 'ArrivalTime', Header: 'Arrival Time'},
-    {accessor: "DestinationAirport", Header: 'Destination Airport'},
-    {accessor: 'StopOverAirport', Header: 'Stop Over', transform: (value: any) => value || '---'},
-    {accessor: 'Price', Header: 'Price', transform: (value: any) => `$${value}`},
+interface ColumnDefinition<T> {
+    accessor: T;
+    Header: string;
+    transform?: (value: string) => string;
+}
+
+const formatDateTime = (value: string): string => new Date(value).toLocaleString('en-AU', {dateStyle: 'short', timeStyle: 'short', hour12: false})
+
+const columns: ColumnDefinition<any>[] = [
+    {accessor: 'airlineCode', Header: 'Airline'},
+    {accessor: 'departureLocation.airport', Header: 'Departure Airport'},
+    {accessor: 'departureTime', Header: 'Departure Time', transform: formatDateTime},
+    {accessor: 'arrivalTime', Header: 'Arrival Time', transform: formatDateTime},
+    {accessor: 'arrivalLocation.airport', Header: 'Destination Airport'},
+    {accessor: 'stopOverLocation.airport', Header: 'Stop Over', transform: (value: any) => value || '---'},
+    {accessor: 'price', Header: 'Price', transform: (value: any) => `$${value}`},
+    {accessor: 'duration', Header: 'Duration', transform: (value: any) => convertMinsToHM(value)},
 ]
 
 const airlines = [
@@ -66,85 +64,40 @@ const airlines = [
     }
 ]
 
-const flights = [
-    {
-        "AirlineName" : "KLM-Royal Dutch Airlines",
-        "FlightNumber" : "KL1601",
-        "DepartureAirport" : "Amsterdam",
-        "DepartureCode" : "AMS",
-        "StopOverAirport" : null,
-        "StopOverCode" : null,
-        "DestinationAirport" : "Rome-Fiumicino",
-        "ArrivalCode" : "FCO",
-        "DepartureTime" : "2022-11-07 09:40:00",
-        "ArrivalTimeStopOver" : null,
-        "DepartureTimeStopOver" : null,
-        "ArrivalTime" : "2022-11-07 11:55:00",
-        "Price" : 1140.11
-    },
-    {
-        "AirlineName" : "Finnair",
-        "FlightNumber" : "AY782",
-        "DepartureAirport" : "Rome-Fiumicino",
-        "DepartureCode" : "FCO",
-        "StopOverAirport" : null,
-        "StopOverCode" : null,
-        "DestinationAirport" : "Helsinki",
-        "ArrivalCode" : "HEL",
-        "DepartureTime" : "2022-10-13 11:20:00",
-        "ArrivalTimeStopOver" : null,
-        "DepartureTimeStopOver" : null,
-        "ArrivalTime" : "2022-10-13 15:45:00",
-        "Price" : 766.96
-    },
-    {
-        "AirlineName" : "Emirates Airlines",
-        "FlightNumber" : "QR31",
-        "DepartureAirport" : "Melbourne",
-        "DepartureCode" : "MEL",
-        "StopOverAirport" : "Doha",
-        "StopOverCode" : "DOH",
-        "DestinationAirport" : "Madrid",
-        "ArrivalCode" : "MAD",
-        "DepartureTime" : "2022-08-27 22:55:00",
-        "ArrivalTimeStopOver" : "2022-08-28 06:10:00",
-        "DepartureTimeStopOver" : "2022-08-28 07:00:00",
-        "ArrivalTime" : "2022-08-28 13:55:00",
-        "Price" : 5385.81
-    },
-    {
-        "AirlineName" : "Delta Air Lines",
-        "FlightNumber" : "DL8",
-        "DepartureAirport" : "San Francisco",
-        "DepartureCode" : "SFO",
-        "StopOverAirport" : "Atlanta",
-        "StopOverCode" : "ATL",
-        "DestinationAirport" : "Dubai",
-        "ArrivalCode" : "DXB",
-        "DepartureTime" : "2022-09-23 13:03:00",
-        "ArrivalTimeStopOver" : "2022-09-23 20:45:00",
-        "DepartureTimeStopOver" : "2022-09-23 21:35:00",
-        "ArrivalTime" : "2022-09-24 19:40:00",
-        "Price" : 1311.99
-    }
-]
+interface Destination {
+    destinationCode: string;
+    airport: string;
+    countryCode: string;
+}
+
+interface Flight {
+    airlineCode: string;
+    flightNumber: string;
+    departureTime: string;
+    departureLocation: Destination;
+    arrivalLocation: Destination;
+    arrivalTime: string;
+    stopOverLocation: Destination;
+    arrivalTimeStopOver: string;
+    departureTimeStopOver: string;
+    price: number;
+    duration: number
+}
 
 
-function convertMsToHM(milliseconds: number) {
-    let seconds = Math.floor(milliseconds / 1000);
-    let minutes = Math.floor(seconds / 60);
+function convertMinsToHM(minutes: number) {
     let hours = Math.floor(minutes / 60);
 
-    seconds = seconds % 60;
-    minutes = seconds >= 30 ? minutes + 1 : minutes;
     minutes = minutes % 60;
-    // hours = hours % 24;
 
     return (hours + " hrs " + minutes + " mins");
 }
 
 export function SearchResultsPage() {
-    const [results, setResults] = useState(flights);
+
+    const { state } = useLocation();
+
+    const [results, setResults] = useState<Flight[]>([]);
     const [sortField, setSortField] = useState('');
     const [ascendingCol, setAscendingCol] = useState('');
     const [descendingCol, setDescendingCol] = useState('');
@@ -153,6 +106,10 @@ export function SearchResultsPage() {
     const [maxPrice, setMaxPrice] = useState(10000);
     const [airlineFilter, setAirlineFilter] = useState('');
     const [durationFilter, setDurationFilter] = useState(180000000);
+
+    useEffect(() => {
+        setResults(state as Flight[])
+    }, [state])
 
     const sortByField = (field: string) => {
         let order = -1;
@@ -283,24 +240,23 @@ export function SearchResultsPage() {
                             </Tr>
                         </Thead>
                         <Tbody>
-                            {results.filter(function(result) {
-                                if (result.Price < minPrice || result.Price > maxPrice) {
+                            {results.filter((result) => {
+                                if (result.price < minPrice || result.price > maxPrice) {
                                     return false;
                                 }
                                 if (airlineFilter !== '') {
-                                    if (airlineFilter !== result.AirlineName) {
+                                    if (airlineFilter !== result.airlineCode) {
                                         return false;
                                     }
                                 }
-                                if ((Date.parse(result.ArrivalTime) - Date.parse(result.DepartureTime)) > durationFilter) {
+                                if ((Date.parse(result.arrivalTime) - Date.parse(result.departureTime)) > durationFilter) {
                                     return false;
                                 }
                                 return true;
                             }).map((result: any) =>
                                 <Tr>
                                     {columns.map((column) =>
-                                        <Td>{column?.transform ? column.transform(result[column.accessor]) : result[column.accessor]}</Td>                                    )}
-                                    <Td>{convertMsToHM(Date.parse(result.ArrivalTime) - Date.parse(result.DepartureTime))}</Td>
+                                        <Td>{column?.transform ? column.transform(_.get(result, column.accessor)) : _.get(result, column.accessor)}</Td>                                    )}
                                     <Td>
                                         <Button type="button"
                                                 colorScheme="red"
