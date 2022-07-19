30c423c6fb1532a7ea669d83773fca56ad41e92f
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-07-19 16:49:47 +1000

    Test Heroku Deploy

--- flightpub-api/src/main/java/seng3150/team4/flightpub/controllers/HomeController.java
+++ flightpub-api/src/main/java/seng3150/team4/flightpub/controllers/HomeController.java
@@ -13,6 +13,6 @@ public class HomeController {
 
     @GetMapping
     public Response Get() {
-        return new StatusResponse(HttpStatus.OK, "Hello World!");
+        return new StatusResponse(HttpStatus.OK, "Hello World! ;)");
     }
 }
