afb7a7c4754dd8e89c1ff18a146a0a6de529ac4e
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-07-20 12:06:06 +1000

    Revert Test commit

--- flightpub-api/src/main/java/seng3150/team4/flightpub/controllers/HomeController.java
+++ flightpub-api/src/main/java/seng3150/team4/flightpub/controllers/HomeController.java
@@ -13,6 +13,6 @@ public class HomeController {
 
     @GetMapping
     public Response Get() {
-        return new StatusResponse(HttpStatus.OK, "Hello World! Testing WebHook");
+        return new StatusResponse(HttpStatus.OK, "Hello World!");
     }
 }
