222510052e6f5d40c09fa7956611700cdd68185f
ChinniganCode <c3331609@uon.edu.au>
2022-07-26 22:03:00 +1000

    Added swagger documentation to backend api, available at localhost:5897/swagger-ui

--- flightpub-api/src/main/java/seng3150/team4/flightpub/configuration/WebConfig.java
+++ flightpub-api/src/main/java/seng3150/team4/flightpub/configuration/WebConfig.java
@@ -3,7 +3,9 @@ package seng3150.team4.flightpub.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
