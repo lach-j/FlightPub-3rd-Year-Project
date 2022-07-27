e7bc00e86351b09f87ef602d77225794bd001cf0
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-07-27 20:06:12 +1000

    SENG-33 - Apply interceptor to all endpoints now that we have Annotation checking

--- flightpub-api/src/main/java/seng3150/team4/flightpub/configuration/WebConfig.java
+++ flightpub-api/src/main/java/seng3150/team4/flightpub/configuration/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(getSecurityFilter())
-            .addPathPatterns("/"); // TODO: add all necessary paths
+            .addPathPatterns("/**");
   }
 
   @Override
