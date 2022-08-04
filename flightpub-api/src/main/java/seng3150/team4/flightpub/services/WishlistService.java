0967fcf57a6f70c5ec1e5484e9a00dae2f354b52
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-08-04 22:29:31 +1000

    SENG-47 - Add service for adding and getting wishlists

---
+++ flightpub-api/src/main/java/seng3150/team4/flightpub/services/WishlistService.java
@@ -0,0 +1,77 @@
+package seng3150.team4.flightpub.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import seng3150.team4.flightpub.domain.models.Destination;
+import seng3150.team4.flightpub.domain.models.Wishlist;
+import seng3150.team4.flightpub.domain.models.WishlistItem;
+import seng3150.team4.flightpub.domain.repositories.IDestinationRepository;
+import seng3150.team4.flightpub.domain.repositories.IWishlistItemRepository;
+import seng3150.team4.flightpub.domain.repositories.IWishlistRepository;
+import seng3150.team4.flightpub.security.CurrentUserContext;
+
+import javax.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class WishlistService implements IWishlistService {
+
+    private final IWishlistRepository wishlistRepository;
+    private final IWishlistItemRepository wishlistItemRepository;
+    private final IUserService userService;
+    private final CurrentUserContext currentUserContext;
+    private final IDestinationRepository destinationRepository;
+
+    @Override
+    public Set<Wishlist> getWishlistForUser(long userId) {
+        var user = userService.getUserByIdSecure(userId);
+        return wishlistRepository.getWishlistsForUser(user);
+    }
+
+    @Override
+    public Wishlist getWishlistById(long wishlistId) {
+        var wishlist = wishlistRepository.findById(wishlistId);
+
+        if (wishlist.isEmpty())
+            throw new EntityNotFoundException(String.format("A wishlist with id %d was not found.", wishlistId));
+
+        return wishlist.get();
+    }
+
+    @Override
+    public Wishlist createWishlist(List<String> destinationCodes) {
+        var user = userService.getUserByIdSecure(currentUserContext.getCurrentUserId());
+
+        var wishlist = new Wishlist();
+
+        wishlist.setUser(user);
+        wishlist.setDateCreated(LocalDateTime.now());
+
+        var wishlistSaved = wishlistRepository.save(wishlist);
+
+        // This isn't ideas as it requires querying in a loop, however it
+        // is the quick way of ensuring the destinations stay in order.
+        var destinations = new ArrayList<Destination>();
+        for (var destinationCode : destinationCodes) {
+            var dest = destinationRepository.findById(destinationCode);
+            dest.ifPresent(destinations::add);
+        }
+
+        Set<WishlistItem> wishlistItems = new HashSet<>();
+        var index = 1;
+        for (var destination : destinations) {
+            var item = new WishlistItem(null, destination, wishlistSaved, index++);
+            wishlistItems.add(item);
+        }
+        wishlistItemRepository.saveAll(wishlistItems);
+        wishlist.setWishlistItems(wishlistItems);
+
+
+        return wishlistRepository.save(wishlist);
+    }
+}
