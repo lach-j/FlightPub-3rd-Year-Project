e85c7e7f9c4cb2856fe5697abe7eb4bcbd6bda8a
ChinniganCode <c3331609@uon.edu.au>
2022-08-04 17:23:22 +1000

    Created basic holidayPackage form input

--- flightpub-ui/src/models/HolidayCardProps.ts
+++ flightpub-ui/src/models/HolidayCardProps.ts
@@ -9,6 +9,16 @@ export interface HolidayCardProps {
   location: string;
   tags: Array<FeatureBadgeProps>;
 }
+export interface HolidayPackage {
+  isPopular: boolean;
+  imageURL: string;
+  packageName: string;
+  packageDescription: string;
+  packageTagline: string;
+  packageNights: number;
+  location: string;
+  price: number;
+}
 
 export interface FeatureBadgeProps {
   tagName: string;
--- flightpub-ui/src/pages/HolidayPackagesPage.tsx
+++ flightpub-ui/src/pages/HolidayPackagesPage.tsx
@@ -1,15 +1,25 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
-import { Box, Button, Center, Grid, Heading, StackDivider, VStack } from '@chakra-ui/react';
+import React, {Dispatch, SetStateAction, SyntheticEvent, useEffect, useState} from 'react';
+import {
+  Box,
+  Button,
+  Center, Checkbox, FormControl, FormErrorMessage, FormLabel,
+  Grid,
+  Heading, Input,
+  Stack,
+  StackDivider,
+  useToast,
+  VStack
+} from '@chakra-ui/react';
 import logo from '../FlightPubLogo.png';
-import { useApi } from '../services/ApiService';
+import {ApiError, useApi} from '../services/ApiService';
 import { endpoints } from '../constants/endpoints';
-import { Airline, ColumnDefinition, Flight, Price } from '../models';
+import {Airline, Flight, Price } from '../models';
 import { Airport, findNearestAirport } from '../utility/geolocation';
-import { convertMinsToHM, formatDateTime } from '../utility/formatting';
-import { NavLink } from 'react-router-dom';
+
+import { NavLink} from 'react-router-dom';
 import { routes } from '../constants/routes';
 import { HolidayCard } from '../components/HolidayCard';
-import { HolidayCardProps } from '../models/HolidayCardProps';
+import { HolidayCardProps, HolidayPackage } from '../models/HolidayCardProps';
 
 export function HolidayPackagesPage({
   cartState
@@ -56,6 +66,66 @@ export function HolidayPackagesPage({
     }
   ];
 
+  const [holidayPackage, setHolidayPackage] = useState<HolidayPackage>({
+    isPopular: true,
+    imageURL:
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/7d/68/1d/four-seasons-resort-whistler.jpg?w=600&h=400&s=1',
+    packageName: 'Iconic Canadian Winter in Whistler',
+    packageDescription:
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore',
+    packageTagline: 'Fun filled adventure in the most iconic part of Canada!',
+    price: 600,
+    packageNights: 7,
+    location: 'Whistler',
+  });
+  const handleHolidayPackageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setHolidayPackage({
+      ...holidayPackage,
+      [event.target.name]: event.target.value
+    });
+  };
+  const handlePackageCreation = (e: SyntheticEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setTimeout(() => {
+      httpPostHolidayPackage('/', holidayPackage) //posts registration request
+          .then(() => {
+            toast({
+              title: 'Holiday Package Created',
+              description:
+                  'Holiday Package Added Successfully!',
+              status: 'success',
+              duration: 9000,
+              isClosable: true,
+              position: 'top'
+            });
+          })
+          .catch((err: ApiError) => {
+            //if an error occurs in registration process
+             {
+              toast({
+                title: 'Error.',
+                description: 'An internal error has occurred, please try again later.',
+                status: 'error',
+                duration: 9000,
+                isClosable: true,
+                position: 'top'
+              });
+            }
+          })
+          .finally(() => setLoading(false));
+      return false;
+    }, 1000);
+  };
+
+
+
+
+
+  const toast = useToast();
+
+  const [loading, setLoading] = useState(false);
+
   //recommended : contains data table of cheapest flights based on user location
   const [recommended, setRecommended] = useState<Flight[]>([]);
 
@@ -70,6 +140,9 @@ export function HolidayPackagesPage({
 
   const { httpGet: httpGetAirlines } = useApi(endpoints.airlines);
   const { httpGet: httpGetRecommended } = useApi(endpoints.recommended);
+  const { httpGet: httpGetHolidayPackages } = useApi(endpoints.holidayPackages);
+  const { httpPost: httpPostHolidayPackage } = useApi(endpoints.createHolidayPackage);
+
 
   //takes price and returns cheapest price value as a string
   const getMinPriceString = (prices: Price[]) => {
@@ -99,9 +172,101 @@ export function HolidayPackagesPage({
     httpGetRecommended('/' + airport.code).then(setRecommended);
   }, [airport]);
 
+
+
   return (
     <Grid>
       <Center>
+        <Box border='2px' borderColor='gray.200' p='10' borderRadius='2xl' w='md'>
+          <form onSubmit={handlePackageCreation}>
+            <Stack spacing='12'>
+              <Box>
+                <Heading>Create Holiday Package</Heading>
+              </Box>
+              <Box>
+                <Stack spacing='3'>
+                  {/* Email input */}
+                  <FormControl isDisabled={loading}>
+                    <FormLabel>isPopular</FormLabel>
+                    <Checkbox
+                        checked={holidayPackage.isPopular}
+                        onChange={handleHolidayPackageChange}
+                    />
+
+                    {/* Firstname input */}
+                  </FormControl>
+                  <FormControl isDisabled={loading}>
+                    <FormLabel>imageUrl</FormLabel>
+                    <Input
+                        name='imageUrl'
+                        value={holidayPackage.imageURL}
+                        onChange={handleHolidayPackageChange}
+                    />
+                    {/* LastName input */}
+                  </FormControl>
+                  <FormControl isDisabled={loading} >
+                    <FormLabel>Package Name</FormLabel>
+                    <Input
+                        name='packageName'
+                        value={holidayPackage.packageName}
+                        onChange={handleHolidayPackageChange}
+                    />
+                  </FormControl>
+                  <FormControl isDisabled={loading} >
+                    <FormLabel>Description</FormLabel>
+                    <Input
+                        name='packageDescription'
+                        value={holidayPackage.packageDescription}
+                        onChange={handleHolidayPackageChange}
+                    />
+                  </FormControl>
+                  <FormControl isDisabled={loading} >
+                    <FormLabel>packageTagline</FormLabel>
+                    <Input
+                        name='packageTagline'
+                        value={holidayPackage.packageTagline}
+                        onChange={handleHolidayPackageChange}
+                    />
+                  </FormControl>
+                  <FormControl isDisabled={loading} >
+                    <FormLabel>packageNights</FormLabel>
+                    <Input
+                        name='packageNights'
+                        value={holidayPackage.packageNights}
+                        onChange={handleHolidayPackageChange}
+                    />
+                  </FormControl>
+                  <FormControl isDisabled={loading} >
+                    <FormLabel>location</FormLabel>
+                    <Input
+                        name='location'
+                        value={holidayPackage.location}
+                        onChange={handleHolidayPackageChange}
+                    />
+                  </FormControl>
+                  <FormControl isDisabled={loading}>
+                    <FormLabel>Password</FormLabel>
+                    <Input
+                        type='number'
+                        name='price'
+                        value={holidayPackage.price}
+                        onChange={handleHolidayPackageChange}
+                    />
+
+                    {/* Error message popup */}
+                    <FormErrorMessage>
+                      Values provided are incorrect
+                    </FormErrorMessage>
+                  </FormControl>
+                </Stack>
+              </Box>
+              {/* Form submission button */}
+              <Button type='submit' isLoading={loading} colorScheme='red'>
+                Create Holiday Package
+              </Button>
+            </Stack>
+          </form>
+        </Box>
         <VStack spacing={2} align='center' divider={<StackDivider borderColor='white' />}>
           <Center backgroundColor='gray.600' maxW='1000px' mx='auto'>
             <img src={logo} alt='Logo' width='1000px' />
@@ -124,6 +289,9 @@ export function HolidayPackagesPage({
           </VStack>
         </VStack>
       </Center>
+      <Button onClick={handlePackageCreation} colorScheme='red'>
+        Create Package
+      </Button>
     </Grid>
   );
 }
