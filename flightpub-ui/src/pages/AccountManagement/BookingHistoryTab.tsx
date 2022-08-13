d406fcc9d523776cbc06547408dcf30c0103a5af
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-08-13 15:36:43 +1000

    Add sorting to booking history

--- flightpub-ui/src/pages/AccountManagement/BookingHistoryTab.tsx
+++ flightpub-ui/src/pages/AccountManagement/BookingHistoryTab.tsx
@@ -1,15 +1,26 @@
-import { Box, Heading, HStack, ListItem, OrderedList, Text, VStack } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import {
+  Box,
+  Heading,
+  HStack,
+  ListItem,
+  OrderedList,
+  Select,
+  Text,
+  VStack
+} from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
 import _ from 'lodash';
 import { useApi } from '../../services/ApiService';
 import { endpoints } from '../../constants/endpoints';
 import { Booking } from '../../models/Booking';
 import { FlightListAccordian } from '../../components/FlightListAccordian';
 import moment from 'moment';
+import { number } from 'yup';
 
 export const BookingHistoryTab = ({ setIsLoading }: { setIsLoading: (value: boolean) => void }) => {
   const { httpGet } = useApi(endpoints.bookings);
   const [bookings, setBookings] = useState<Booking[]>([]);
+  const [sortFunction, setSortFunction] = useState<string>('');
 
   useEffect(() => {
     httpGet('').then((bookings) => {
@@ -17,11 +28,42 @@ export const BookingHistoryTab = ({ setIsLoading }: { setIsLoading: (value: bool
     });
   }, []);
 
+  const getSortMethod = () => {
+    switch (sortFunction) {
+      case 'bookedDesc':
+        return (a: Booking, b: Booking) =>
+          moment(a.dateBooked).isBefore(moment(b.dateBooked)) ? 1 : -1;
+
+      case 'bookedAsc':
+        return (a: Booking, b: Booking) =>
+          moment(a.dateBooked).isBefore(moment(b.dateBooked)) ? -1 : 1;
+
+      case 'priceAsc':
+        return (a: Booking, b: Booking) =>
+          a.flights.reduce((a, b) => a + b.prices[0].price, 0) -
+          b.flights.reduce((a, b) => a + b.prices[0].price, 0);
+
+      case 'priceDesc':
+        return (a: Booking, b: Booking) =>
+          b.flights.reduce((a, b) => a + b.prices[0].price, 0) -
+          a.flights.reduce((a, b) => a + b.prices[0].price, 0);
+    }
+    return () => 1;
+  };
+
   return (
     <>
       <Heading mb='1em'>Booking History</Heading>
+      <Box mb='4'>
+        <Select onChange={(e) => setSortFunction(e.target.value)} w='fit-content'>
+          <option value='bookedDesc'>Newest</option>
+          <option value='bookedAsc'>Oldest</option>
+          <option value='priceAsc'>Cheapest</option>
+          <option value='priceDesc'>Most Expensive</option>
+        </Select>
+      </Box>
       <VStack gap='4'>
-        {bookings.map((booking) => {
+        {bookings.sort(getSortMethod()).map((booking) => {
           const date =
             booking?.dateBooked instanceof Date
               ? booking?.dateBooked
