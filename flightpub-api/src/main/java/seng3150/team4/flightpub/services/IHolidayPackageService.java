0b69b4257e590966ccfaa27ebf658099669ea0e8
ChinniganCode <79070276+ChinniganCode@users.noreply.github.com>
2022-08-13 16:19:24 +1000

    Update IHolidayPackageService.java

--- flightpub-api/src/main/java/seng3150/team4/flightpub/services/IHolidayPackageService.java
+++ flightpub-api/src/main/java/seng3150/team4/flightpub/services/IHolidayPackageService.java
@@ -1,7 +1,7 @@
 package seng3150.team4.flightpub.services;
 
 import seng3150.team4.flightpub.domain.models.HolidayPackage;
-
+import java.util.Set;
 public interface IHolidayPackageService {
     HolidayPackage makePackage(HolidayPackage holidayPackage, Set<Long> flightIds);
 
