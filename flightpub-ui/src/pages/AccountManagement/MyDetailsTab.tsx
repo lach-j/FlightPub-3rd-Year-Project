55215b5ec3928e40f8d9732b30af47b1fd562f33
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-08-14 21:44:28 +1000

    SENG-121 - Refactor to reuse user details form

--- flightpub-ui/src/pages/AccountManagement/MyDetailsTab.tsx
+++ flightpub-ui/src/pages/AccountManagement/MyDetailsTab.tsx
@@ -37,11 +37,67 @@ const editProfileForm: {
 
 export const MyDetailsTab = ({ setIsLoading }: { setIsLoading: (value: boolean) => void }) => {
   const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+  const toast = useToast();
 
+  const handlePostDelete = () => {
+    toast({
+      title: 'Account Deleted',
+      description: 'Your account was deleted successfully, you have been logged out permanently.',
+      status: 'success',
+      duration: 9000,
+      isClosable: true,
+      position: 'top'
+    });
+    localStorage.removeItem('bearer-token');
+    localStorage.removeItem('user-id');
+    setUser(null);
+    navigate(routes.default);
+  };
+
+  const handlePostSave = (savedUser: User) => {
+    setUser(savedUser);
+    toast({
+      title: 'Details updated',
+      description: 'Your account details have been updated successfully.',
+      status: 'success',
+      duration: 3000,
+      isClosable: true,
+      position: 'top'
+    });
+  };
+
+  return (
+    <>
+      <Heading mb='1em'>My Details</Heading>
+      {user && (
+        <UserDetailsForm
+          setIsLoading={setIsLoading}
+          user={user}
+          onDelete={handlePostDelete}
+          onSave={handlePostSave}
+        />
+      )}
+    </>
+  );
+};
+
+export const UserDetailsForm = ({
+  setIsLoading,
+  user,
+  onDelete,
+  onSave,
+  showRole = false
+}: {
+  setIsLoading: (value: boolean) => void;
+  user: User;
+  onDelete: () => void;
+  onSave: (user: User) => void;
+  showRole?: boolean;
+}) => {
   const { httpPatch, httpDelete } = useApi(endpoints.users);
 
   const toast = useToast();
-  const navigate = useNavigate();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const cancelRef = React.useRef(null);
 
@@ -58,19 +114,7 @@ export const MyDetailsTab = ({ setIsLoading }: { setIsLoading: (value: boolean) 
 
     httpDelete(`/${user?.id}`)
       .then(() => {
-        toast({
-          title: 'Account Deleted',
-          description:
-            'Your account was deleted successfully, you have been logged out permanently.',
-          status: 'success',
-          duration: 9000,
-          isClosable: true,
-          position: 'top'
-        });
-        localStorage.removeItem('bearer-token');
-        localStorage.removeItem('user-id');
-        setUser(null);
-        navigate(routes.default);
+        onDelete();
       })
       .finally(() => {
         setIsLoading(false);
@@ -85,15 +129,7 @@ export const MyDetailsTab = ({ setIsLoading }: { setIsLoading: (value: boolean) 
 
     httpPatch(`/${user?.id}`, { firstName, lastName, email })
       .then((res) => {
-        setUser(res);
-        toast({
-          title: 'Details updated',
-          description: 'Your account details have been updated successfully.',
-          status: 'success',
-          duration: 3000,
-          isClosable: true,
-          position: 'top'
-        });
+        onSave(res);
         setIsLoading(false);
       })
       .catch(() => {
@@ -117,7 +153,6 @@ export const MyDetailsTab = ({ setIsLoading }: { setIsLoading: (value: boolean) 
 
   return (
     <>
-      <Heading mb='1em'>My Details</Heading>
       <form>
         <VStack gap='1em'>
           {editProfileForm.inputs.map((input) => (
@@ -152,7 +187,7 @@ export const MyDetailsTab = ({ setIsLoading }: { setIsLoading: (value: boolean) 
             </AlertDialogHeader>
 
             <AlertDialogBody>
-              Are you sure you want to delete your account? This action cannot be undone.
+              Are you sure you want to delete this account? This action cannot be undone.
             </AlertDialogBody>
 
             <AlertDialogFooter>
--- flightpub-ui/src/pages/AdminPortal/ManageUserTab.tsx
+++ flightpub-ui/src/pages/AdminPortal/ManageUserTab.tsx
@@ -13,13 +13,16 @@ import {
   useDisclosure,
   useToast,
   VStack,
-  Input
+  Input,
+  Select,
+  Box,
+  Text
 } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { CustomEditible } from '../../components/CustomEditable';
 import { routes } from '../../constants/routes';
 import { useNavigate } from 'react-router-dom';
-import { Airline, ColumnDefinition, Flight, Price } from '../../models';
+import { Airline, ColumnDefinition, Flight, Price, User } from '../../models';
 import { useApi } from '../../services/ApiService';
 import {
   AutoComplete,
@@ -27,148 +30,58 @@ import {
   AutoCompleteItem,
   AutoCompleteList
 } from '@choc-ui/chakra-autocomplete';
+import _ from 'lodash';
+import { UserDetailsForm } from '../AccountManagement/MyDetailsTab';
+import { endpoints } from '../../constants/endpoints';
 
 const editProfileForm: {
-  inputs: Array<{ label: string; name: string; type?: string }>;
+  inputs: Array<{ label: string; name: keyof User; type?: string }>;
 } = {
   inputs: [
     { label: 'Email', name: 'email' },
-    { label: 'First Name', name: 'fname' },
-    { label: 'Last name', name: 'lname' },
-    { label: 'Phone Number', name: 'ph', type: 'tel' }
+    { label: 'First Name', name: 'firstName' },
+    { label: 'Last name', name: 'lastName' }
+    // { label: 'Phone Number', name: 'ph', type: 'tel' }
   ]
 };
 
-const handleDiscardChanges = () => {
-  // TODO : replace this with real refresh logic
-  window.location.reload();
-};
-
 export const ManageUserTab = ({ setIsLoading }: { setIsLoading: (value: boolean) => void }) => {
-  const [userData, setUserData] = useState<any>({
-    email: 'user@example.com',
-    fname: 'Lachlan',
-    lname: 'Johnson',
-    ph: '+6112345678'
-  });
-  const handleDetailsUpdate = (field: string, value: string) => {
-    if (value === userData[field]) return;
-    setUserData({ ...userData, [field]: value });
-    setIsDirty(true);
+  const [user, setUser] = useState<User | undefined>();
+  const [userIdentifier, setUserIdentifier] = useState('');
+
+  const { httpGet } = useApi(endpoints.users);
+
+  const searchUser = () => {
+    httpGet(`/${userIdentifier}`).then((user) => {
+      setUser(user);
+    });
   };
-  const [isDirty, setIsDirty] = useState<boolean>(false);
-  const navigate = useNavigate();
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const toast = useToast();
-  const cancelRef = React.useRef(null);
-  const [hidden, setHidden] = useState(true);
-  const handleSaveChanges = () => {
-    setIsLoading(true);
-    // Simulate api delay with timeout
-    setTimeout(() => {
-      toast({
-        title: 'Details updated',
-        description: 'Your account details have been updated successfully.',
-        status: 'success',
-        duration: 3000,
-        isClosable: true,
-        position: 'top'
-      });
-      setIsLoading(false);
-      setIsDirty(false);
-    }, 2000);
-  };
-  const handleDelete = () => {
-    setIsLoading(true);
-    // Simulate api delay with timeout
-    setTimeout(() => {
-      toast({
-        title: 'Account Deleted',
-        description: 'Your account was deleted successfully, you have been logged out permanently.',
-        status: 'success',
-        duration: 9000,
-        isClosable: true,
-        position: 'top'
-      });
-      setIsLoading(false);
-      navigate(routes.default);
-    }, 2000);
-  };
+
   return (
     <>
-      <Heading mb='1em'>Manage Users</Heading>
-      {!hidden ? (
-        <div>
-          {/*<AutoComplete*/}
-          {/*    openOnFocus*/}
-          {/*    onChange={(value =>*/}
-          {/*            handleSearchQueryUpdate*/}
-          {/*    */}
-          {/*    )}*/}
-
-          {/*>*/}
-          <Button onClick={() => setHidden((s) => !s)} ml={3}>
-            Search
-          </Button>
-        </div>
-      ) : null}
-
-      {hidden ? (
-        <div>
-          <Heading mb='1em'>My Details</Heading>
-          <form>
-            <VStack gap='1em'>
-              {editProfileForm.inputs.map((input) => (
-                <CustomEditible
-                  name={input.name}
-                  value={userData?.[input.name]}
-                  label={input.label}
-                  type={input?.type || 'text'}
-                  onSave={(value) => handleDetailsUpdate(input.name, value)}
-                />
-              ))}
-              <HStack w='full' gap='1em'>
-                <Button colorScheme={'blue'} disabled={!isDirty} onClick={handleSaveChanges}>
-                  Save
-                </Button>
-                <Button colorScheme={'gray'} disabled={!isDirty} onClick={handleDiscardChanges}>
-                  Discard Changes
-                </Button>
-              </HStack>
-            </VStack>
-          </form>
-          <Divider mt='2em' mb='3em' />
-          <Button colorScheme='red' variant='outline' onClick={onOpen}>
-            Delete Account
-          </Button>
-
-          <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
-            <AlertDialogOverlay>
-              <AlertDialogContent>
-                <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-                  Delete Account
-                </AlertDialogHeader>
-
-                <AlertDialogBody>
-                  Are you sure you want to delete your account? This action cannot be undone.
-                </AlertDialogBody>
-
-                <AlertDialogFooter>
-                  <Button ref={cancelRef} onClick={onClose}>
-                    Cancel
-                  </Button>
-                  <Button colorScheme='red' onClick={handleDelete} ml={3}>
-                    Delete
-                  </Button>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialogOverlay>
-          </AlertDialog>
-          <Button onClick={() => setHidden((s) => !s)} ml={3}>
-            Search
-          </Button>
-        </div>
-      ) : null}
+      <HStack>
+        <Heading mb='0.5em'>Manage Users</Heading>
+        <HStack w='full'>
+          <Box whiteSpace='nowrap'>Find user by</Box>
+          <Select>
+            <option>Id</option>
+            <option>Email</option>
+          </Select>
+          <Input onChange={(e) => setUserIdentifier(e.target.value)} />
+          <Button onClick={searchUser}>Search</Button>
+        </HStack>
+      </HStack>
+      {user ? (
+        <UserDetailsForm
+          setIsLoading={() => {}}
+          onDelete={() => {}}
+          onSave={() => {}}
+          user={user}
+          showRole
+        />
+      ) : (
+        <Text>Select a user to edit their details.</Text>
+      )}
     </>
   );
 };
