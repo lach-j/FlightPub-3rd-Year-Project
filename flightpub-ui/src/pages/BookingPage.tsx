50351ed190803584547c01356c092d90542cadc5
keeno2311 <84700725+keeno2311@users.noreply.github.com>
2022-08-14 12:52:08 +1000

    Added fully functional form validation on booking page

--- flightpub-ui/src/pages/BookingPage.tsx
+++ flightpub-ui/src/pages/BookingPage.tsx
@@ -33,7 +33,8 @@ import React, {
   SyntheticEvent,
   useEffect,
   useState,
-  useContext
+  useContext,
+  ChangeEvent
 } from 'react';
 import { useApi } from '../services/ApiService';
 import { ApiError } from '../services/ApiService';
@@ -51,6 +52,31 @@ import { FlightListAccordian } from '../components/FlightListAccordian';
 import { PaymentDetailsForm } from '../components/PaymentDetailsForm';
 import { TypeOf } from 'yup';
 import * as yup from 'yup';
+import { ObjectShape } from 'yup/lib/object';
+
+export interface BillingDetails {
+  firstName: string,
+  lastName: string,
+  company: string,
+  addressLine1: string,
+  addressLine2: string,
+  city: string,
+  postCode: string,
+  state: string,
+}
+
+export interface CardDetails {
+  cardNumber: string,
+  cardholder: string,
+  expiryDate: string,
+  ccv: string,
+}
+
+export interface DirectDebitDetails {
+  bsb: string,
+  accountNumber: string,
+  accountName: string,
+}
 
 export const BookingPage = ({
   cartState
@@ -64,10 +90,34 @@ export const BookingPage = ({
   const [paymentType, setPaymentType] = useState<SavedPaymentType | undefined>();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [bookingRequest, setBookingRequest] = useState<any>({
-    userId: 0,
+    userId: 2,
     flightIds: [],
     passengers: []
   });
+  
+  const [billingForm, setBillingForm] = useState<BillingDetails>({
+    firstName: "",
+    lastName: "",
+    company: "",
+    addressLine1: "",
+    addressLine2: "",
+    city: "",
+    postCode: "",
+    state: "",
+  });
+  const [cardForm, setCardForm] = useState<CardDetails>({
+    cardNumber: "",
+    cardholder: "",
+    expiryDate: "",
+    ccv: "",
+  });
+  const [directDebitForm, setDirectDebitForm] = useState<DirectDebitDetails>({
+    bsb: "",
+    accountNumber: "",
+    accountName: "",
+  });
+  const [payPalEmail, setPayPalEmail] = useState<string>("");
+
   const toast = useToast();
   const { httpPost } = useApi(endpoints.book);
   const [cart, setCart] = cartState;
@@ -110,7 +160,7 @@ export const BookingPage = ({
       .string()
       .matches(/^\d{16}$/)
       .required(),
-    cardHolderName: yup
+    cardholder: yup
       .string()
       .matches(/[a-zA-Z]/)
       .required(),
@@ -118,7 +168,7 @@ export const BookingPage = ({
       .string()
       .matches(/^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/)
       .required(),
-    cvv: yup
+    ccv: yup
       .string()
       .matches(/^\d{3}$/)
       .required(),
@@ -142,8 +192,126 @@ export const BookingPage = ({
     email: yup.string().email('must be a valid email').required(),
   });
 
-  const handleBooking = (e: SyntheticEvent) => {
-    e.preventDefault();
+  const submitEvent = async () => {
+    let success = true;
+    let pType = paymentType?.toString();
+    if (pType) {
+      let billingDetails = {
+        firstName: billingForm.firstName,
+        lastName: billingForm.lastName,
+        company: billingForm.company,
+        addressLine1: billingForm.addressLine1,
+        addressLine2: billingForm.addressLine2,
+        city: billingForm.city,
+        postCode: billingForm.postCode,
+        state: billingForm.state,
+      }
+
+      if (!(await validateSchema(billingDetailsSchema, billingDetails))){
+        success = false;
+        toast({
+          title: 'Error!',
+          description: 'Invalid billing details. Please try again!',
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+          position: 'top'
+        });
+      }
+
+      var paymentDetails;
+
+      switch(paymentType?.toString()){
+        case 'CARD':
+          paymentDetails = {
+            cardNumber: cardForm.cardNumber,
+            cardholder: cardForm.cardholder,
+            expiryDate: cardForm.expiryDate,
+            ccv: cardForm.ccv,
+          }
+          if (!(await validateSchema(cardDetailsSchema, paymentDetails))){
+            success = false;
+            toast({
+              title: 'Error!',
+              description: 'Invalid card details. Please try again!',
+              status: 'error',
+              duration: 9000,
+              isClosable: true,
+              position: 'top'
+            });
+          }
+          break;
+        case 'DIRECT_DEBIT':
+          paymentDetails = {
+           bsb: directDebitForm.bsb,
+           accountNumber: directDebitForm.accountNumber,
+           accountName: directDebitForm.accountName,
+          }
+          if (!(await validateSchema(directDebitDetailsSchema, paymentDetails))){
+            success = false;
+            toast({
+              title: 'Error!',
+              description: 'Invalid direct debit details. Please try again!',
+              status: 'error',
+              duration: 9000,
+              isClosable: true,
+              position: 'top'
+            });
+          }
+          break;
+        case 'PAYPAL':
+          paymentDetails = {
+            email: payPalEmail,
+          }
+          if (!(await validateSchema(paypalSchema, paymentDetails))){
+            success = false;
+            toast({
+              title: 'Error!',
+              description: 'Invalid paypal email. Please try again!',
+              status: 'error',
+              duration: 9000,
+              isClosable: true,
+              position: 'top'
+            });
+          }
+          break;
+      }
+    } else {
+      success = false;
+      toast({
+        title: 'Error!',
+        description: 'Please select a payment type before proceeding!',
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+        position: 'top'
+      });
+    }
+    return success;
+  }
+
+  const validateSchema = async (schema: yup.ObjectSchema<ObjectShape, any>, value: any) => {
+    return await schema.isValid(value);
+  }
+
+  const handleBillingChange = (fieldValue: keyof BillingDetails) => (e: ChangeEvent<HTMLInputElement>) => {
+      setBillingForm(billingForm => ({...billingForm, [fieldValue]: e.target.value}));
+    }
+
+  const handleCardChange = (field: string,  value: any) => {
+      setCardForm(cardForm => ({...cardForm, [field]: value}));
+    }
+
+  const handleDirectDebitChange = (field: string, value: any) => {
+    setDirectDebitForm(directDebitForm => ({...directDebitForm, [field]: value}));
+  }
+
+  const handlePayPalChange = (value: any) => {
+    if (typeof value == 'string')
+      setPayPalEmail(value);
+  }
+
+  const handleBooking = () => {
     onOpen();
     httpPost('', bookingRequest)
       .then(() => {
@@ -176,6 +344,17 @@ export const BookingPage = ({
 
   const onPaymentFieldChange = (field: string, value: any) => {
     setBookingRequest((br: any) => ({ ...br, payment: { ...br.payment, [field]: value } }));
+    switch(paymentType?.toString()){
+      case 'CARD':
+        handleCardChange(field, value);
+        break;
+      case 'DIRECT_DEBIT':
+        handleDirectDebitChange(field, value);
+        break;
+      case 'PAYPAL':
+        handlePayPalChange(value);
+        break;
+    }
   };
 
   useEffect(() => {
@@ -210,33 +389,33 @@ export const BookingPage = ({
             <HStack w='full'>
               <FormControl flex={1}>
                 <FormLabel>First Name</FormLabel>
-                <Input />
+                <Input onChange={handleBillingChange('firstName')}/>
               </FormControl>
               <FormControl flex={1}>
                 <FormLabel>Last Name</FormLabel>
-                <Input />
+                <Input onChange={handleBillingChange('lastName')}/>
               </FormControl>
             </HStack>
             <FormControl>
               <FormLabel>Company (optional)</FormLabel>
-              <Input />
+              <Input onChange={handleBillingChange('company')}/>
             </FormControl>
             <FormControl>
               <FormLabel>Address line 1</FormLabel>
-              <Input />
+              <Input onChange={handleBillingChange('addressLine1')}/>
             </FormControl>
             <FormControl>
               <FormLabel>Address line 2</FormLabel>
-              <Input />
+              <Input onChange={handleBillingChange('addressLine2')}/>
             </FormControl>
             <HStack w='full'>
               <FormControl flex={2}>
                 <FormLabel>City</FormLabel>
-                <Input />
+                <Input onChange={handleBillingChange('city')}/>
               </FormControl>
               <FormControl flex={1}>
                 <FormLabel>Postcode</FormLabel>
-                <Input />
+                <Input onChange={handleBillingChange('postCode')}/>
               </FormControl>
             </HStack>
             <HStack w='full'>
@@ -250,7 +429,7 @@ export const BookingPage = ({
               </FormControl>
               <FormControl flex={1}>
                 <FormLabel>State</FormLabel>
-                <Input />
+                <Input onChange={handleBillingChange('state')}/>
               </FormControl>
             </HStack>
           </VStack>
@@ -283,7 +462,11 @@ export const BookingPage = ({
             <Switch mt='2em'>Save payment for future transactions?</Switch>
           )}
           <HStack w='full' gap='1em' mt='2em'>
-            <Button onClick={handleBooking} colorScheme='red'>
+            <Button onClick={async () => {
+              if(await submitEvent())
+                handleBooking();
+              }} 
+              colorScheme='red'>
               Book Now
             </Button>
             <Button as={NavLink} to={routes.search} colorScheme='gray'>
