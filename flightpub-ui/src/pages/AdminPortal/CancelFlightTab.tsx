6444b87fabba2651acc1a8bfbb2bcd7ae50e54b3
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-08-14 20:41:42 +1000

    fix invalid imports

--- flightpub-ui/src/pages/AdminPortal/CancelFlightTab.tsx
+++ flightpub-ui/src/pages/AdminPortal/CancelFlightTab.tsx
@@ -1,114 +1,117 @@
 import {
-    AlertDialog,
-    AlertDialogBody,
-    AlertDialogContent,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogOverlay,
-    Button, Center,
-    Divider,
-    Heading,
-    HStack,
-    useDisclosure,
-    useToast,
-    VStack,
-    Input,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Center,
+  Divider,
+  Heading,
+  HStack,
+  useDisclosure,
+  useToast,
+  VStack,
+  Input
 } from '@chakra-ui/react';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { CustomEditible } from '../../components/CustomEditable';
 import { routes } from '../../constants/routes';
 import { useNavigate } from 'react-router-dom';
-import {Airline, ColumnDefinition, Flight, Price} from "../../models";
-import {httpGet} from "../../services/ApiService";
-import {endpoints} from "../../constants/endpoints";
-import {ResultsTable} from "../../components/CancelFlightsTable";
-import {convertMinsToHM, formatDateTime} from "../../utility/formatting";
-import {airlines} from "../../data/airline";
-import {Airport, findNearestAirport} from "../../utility/geolocation";
+import { Airline, ColumnDefinition, Flight, Price } from '../../models';
+import { useApi } from '../../services/ApiService';
+import { endpoints } from '../../constants/endpoints';
+import { ResultsTable } from '../../components/CancelFlightsTable';
+import { convertMinsToHM, formatDateTime } from '../../utility/formatting';
+import { airlines } from '../../data/airline';
+import { Airport, findNearestAirport } from '../../utility/geolocation';
 
+export const CancelFlightTab = ({ setIsLoading }: { setIsLoading: (value: boolean) => void }) => {
+  //recommended : contains data table of cheapest flights based on user location
+  const [recommended, setRecommended] = useState<Flight[]>([]);
 
-export const CancelFlightTab = ({ setIsLoading }: { setIsLoading: (value: boolean) => void } ) => {
-    //recommended : contains data table of cheapest flights based on user location
-    const [recommended, setRecommended] = useState<Flight[]>([]);
+  //userLocation: Uses geoLocation to store users current position
+  const [userLocation, setUserLocation] = useState<any>();
 
-    //userLocation: Uses geoLocation to store users current position
-    const [userLocation, setUserLocation] = useState<any>();
+  //airport: User's nearest airport for reccomendations
+  const [airport, setAirport] = useState<Airport | undefined>();
 
-    //airport: User's nearest airport for reccomendations
-    const [airport, setAirport] = useState<Airport | undefined>();
+  //airlines : list of all airlines from models/Airline
+  const [airlines, setAirlines] = useState<Airline[]>([]);
 
-    //airlines : list of all airlines from models/Airline
-    const [airlines, setAirlines] = useState<Airline[]>([]);
+  const [idFilter, setIdFilter] = useState<number>();
 
-    const [idFilter, setIdFilter] = useState<number>();
+  const { httpGet } = useApi(endpoints.airlines);
 
+  //takes price and returns cheapest price value as a string
+  const getMinPriceString = (prices: Price[]) => {
+    if (!prices) return '---';
+    let pricesVals = prices.map((p) => p.price);
+    let minPrice = Math.min(...pricesVals);
+    return `$${minPrice}`;
+  };
 
-    //takes price and returns cheapest price value as a string
-    const getMinPriceString = (prices: Price[]) => {
-        if (!prices) return '---';
-        let pricesVals = prices.map(p => p.price);
-        let minPrice = Math.min(...pricesVals);
-        return `$${minPrice}`;
-    };
+  //Gets users current position and retrieves list of airlines
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => setUserLocation(position.coords));
+    httpGet('').then(setAirlines);
+  }, []);
 
-    //Gets users current position and retrieves list of airlines
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(position => setUserLocation(position.coords));
-        httpGet(endpoints.airlines)
-            .then(setAirlines);
-    }, []);
+  //Takes user location and finds nearest airport on load
+  useEffect(() => {
+    if (!userLocation) return;
+    let airport = findNearestAirport([userLocation.longitude, userLocation.latitude]);
+    setAirport(airport);
+  }, [userLocation]);
 
-    //Takes user location and finds nearest airport on load
-    useEffect(() => {
-        if (!userLocation) return;
-        let airport = findNearestAirport([userLocation.longitude, userLocation.latitude]);
-        setAirport(airport);
-    }, [userLocation]);
+  //Filters resultdata based on stored filter criteria
+  const filterResults = (result: Flight) => {
+    if (idFilter && result.id !== idFilter) return false;
 
+    return true;
+  };
 
+  //filtering by duration
+  const filterByID = (val: number) => {
+    setIdFilter(val);
+  };
 
-    //Filters resultdata based on stored filter criteria
-    const filterResults = (result: Flight) => {
+  // Defines columns for DataTable in correct format
+  const columns: ColumnDefinition<any>[] = [
+    {
+      accessor: 'id',
+      Header: 'Unique Id'
+    },
+    {
+      accessor: 'airlineCode',
+      Header: 'Airline',
+      transform: (value) => airlines.find((a) => a.airlineCode === value)?.airlineName || value
+    },
+    { accessor: 'departureLocation.airport', Header: 'Departure Airport' },
+    { accessor: 'departureTime', Header: 'Departure Time', transform: formatDateTime },
+    { accessor: 'arrivalTime', Header: 'Arrival Time', transform: formatDateTime },
+    { accessor: 'arrivalLocation.airport', Header: 'Destination Airport' },
+    {
+      accessor: 'stopOverLocation.airport',
+      Header: 'Stop Over',
+      transform: (value: any) => value || '---'
+    },
+    { accessor: 'prices', Header: 'Price', transform: getMinPriceString },
+    { accessor: 'duration', Header: 'Duration', transform: (value: any) => convertMinsToHM(value) }
+  ];
 
-        if (idFilter && result.id !== idFilter) return false;
-
-        return true;
-    };
-
-
-    //filtering by duration
-    const filterByID = (val: number) => {
-        setIdFilter(val);
-    };
-
-// Defines columns for DataTable in correct format
-    const columns: ColumnDefinition<any>[] = [
-        {
-            accessor: 'id',
-            Header: 'Unique Id',
-        },
-        {
-            accessor: 'airlineCode',
-            Header: 'Airline',
-            transform: value => airlines.find(a => a.airlineCode === value)?.airlineName || value,
-        },
-        { accessor: 'departureLocation.airport', Header: 'Departure Airport' },
-        { accessor: 'departureTime', Header: 'Departure Time', transform: formatDateTime },
-        { accessor: 'arrivalTime', Header: 'Arrival Time', transform: formatDateTime },
-        { accessor: 'arrivalLocation.airport', Header: 'Destination Airport' },
-        { accessor: 'stopOverLocation.airport', Header: 'Stop Over', transform: (value: any) => value || '---' },
-        { accessor: 'prices', Header: 'Price', transform: getMinPriceString },
-        { accessor: 'duration', Header: 'Duration', transform: (value: any) => convertMinsToHM(value) },
-    ];
-
-    return (
-        <>
-            <Heading mb='1em'>Cancel Flight</Heading>
-            <Input placeholder='Flight id' size='md' onChange = { (val) => filterByID } />
-            <Center>
-                <ResultsTable columns={columns} data={recommended.filter(filterResults)} keyAccessor='id'>
-                </ResultsTable>
-            </Center>
-        </>
-    )
-}
+  return (
+    <>
+      <Heading mb='1em'>Cancel Flight</Heading>
+      <Input placeholder='Flight id' size='md' onChange={(val) => filterByID} />
+      <Center>
+        <ResultsTable
+          columns={columns}
+          data={recommended.filter(filterResults)}
+          keyAccessor='id'
+        ></ResultsTable>
+      </Center>
+    </>
+  );
+};
