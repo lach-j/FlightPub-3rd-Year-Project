86d7481c567169ed7bfd7e7dfe02bc9c21fd3a4d
ChinniganCode <c3331609@uon.edu.au>
2022-08-15 16:46:34 +1000

    Updating HolidayPackageBookingRequest to take: - holidayPackageId - Set of passengers - sent of flightIds - a payment
    From this it creates a flightbooking and relates this flightbooking to the holidaypackagebooking

--- flightpub-api/src/main/java/seng3150/team4/flightpub/domain/models/HolidayPackageBooking.java
+++ flightpub-api/src/main/java/seng3150/team4/flightpub/domain/models/HolidayPackageBooking.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Table(name = "HolidayPackageBooking")
 @Entity
@@ -17,16 +19,25 @@ public class HolidayPackageBooking implements IEntity {
     @Column(name = "Id")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-    @Column(name = "UserId")
-    private long userId;
+    @ManyToOne
+    @JoinColumn(name = "UserId")
+    private User user;
 
     @JsonBackReference
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "HolidayPackageId", nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "HolidayPackageId", referencedColumnName = "Id")
     private HolidayPackage holidayPackage;
 
     @Column(name = "DateBooked")
     private LocalDateTime dateBooked;
+
+    @ManyToOne
+    @JoinColumn(name = "PaymentId", referencedColumnName = "Id")
+    private Payment payment;
+
+    @ManyToOne
+    @JoinColumn(name = "BookingId", referencedColumnName = "Id")
+    private Booking booking;
 }
--- flightpub-api/src/main/java/seng3150/team4/flightpub/services/HolidayPackageBookingService.java
+++ flightpub-api/src/main/java/seng3150/team4/flightpub/services/HolidayPackageBookingService.java
@@ -2,15 +2,15 @@ package seng3150.team4.flightpub.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import seng3150.team4.flightpub.domain.models.Booking;
-import seng3150.team4.flightpub.domain.models.Flight;
-import seng3150.team4.flightpub.domain.models.HolidayPackageBooking;
+import seng3150.team4.flightpub.domain.models.*;
 import seng3150.team4.flightpub.domain.repositories.IBookingRepository;
 import seng3150.team4.flightpub.domain.repositories.IFlightRepository;
 import seng3150.team4.flightpub.domain.repositories.IHolidayPackageBookingRepository;
 import seng3150.team4.flightpub.domain.repositories.IHolidayPackageRepository;
 
 import javax.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -22,13 +22,22 @@ public class HolidayPackageBookingService implements IHolidayPackageBookingServi
 
     private final IBookingRepository bookingRepository;
     private final IHolidayPackageRepository holidayPackageRepository;
+    private final IUserService userService;
     private final IHolidayPackageBookingRepository holidayPackageBookingRepository;
 
 
     @Override
-    public HolidayPackageBooking makeHolidayPackageBooking(HolidayPackageBooking holidayPackageBooking) {
-        HolidayPackageBooking saveHolidayPackageBooking = holidayPackageBookingRepository.save(holidayPackageBooking);
-        return saveHolidayPackageBooking;
+    public HolidayPackageBooking makeHolidayPackageBooking(long holidayPackageId, long userId, Payment payment, Booking booking) {
+        var holidayPackageBooking = new HolidayPackageBooking();
+        holidayPackageBooking.setDateBooked(LocalDateTime.now(ZoneOffset.UTC));
+
+        var user = userService.getUserByIdSecure(userId);
+        holidayPackageBooking.setUser(user);
+        holidayPackageBooking.setPayment(payment);
+        holidayPackageBooking.setBooking(booking);
+        var holpackage = holidayPackageRepository.findById(holidayPackageId).orElse(null);
+        holidayPackageBooking.setHolidayPackage(holpackage);
+        return holidayPackageBookingRepository.save(holidayPackageBooking);
     }
 
     @Override
