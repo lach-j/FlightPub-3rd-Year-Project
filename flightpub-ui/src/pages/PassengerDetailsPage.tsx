128905b7f9cf6dfa8e4c66a9ff161380ff5533d4
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-08-16 19:30:11 +1000

    Formatting

--- flightpub-ui/src/pages/PassengerDetailsPage.tsx
+++ flightpub-ui/src/pages/PassengerDetailsPage.tsx
@@ -64,14 +64,13 @@ export const PassengerDetailsPage = ({
   const { user, setUser } = useContext(UserContext);
 
   const passengerSchema = yup.object().shape({
-    firstName: yup
+    firstName: yup.string().required('First name is required'),
+    lastName: yup.string().required('Last name is required'),
+    email: yup.string().email('Email must be a valid email address').required('Email is required'),
+    confEmail: yup
       .string()
-      .required("First name is required"),
-    lastName: yup
-      .string()
-      .required("Last name is required"),
-    email: yup.string().email('Email must be a valid email address').required("Email is required"),
-    confEmail: yup.string().email('Confirm email must be a valid email address').required("Confirm email is required")
+      .email('Confirm email must be a valid email address')
+      .required('Confirm email is required')
   });
 
   const submitEvent = async () => {
@@ -83,7 +82,7 @@ export const PassengerDetailsPage = ({
         email: emails[i],
         confEmail: confEmails[i]
       };
-      if (!(await validatePassenger(pass, (i + 1)))) {
+      if (!(await validatePassenger(pass, i + 1))) {
         success = false;
       }
     }
@@ -94,20 +93,20 @@ export const PassengerDetailsPage = ({
     let success = true;
     let errorArray: string[] = [];
     try {
-        await passengerSchema.validate(passenger, { abortEarly: false });
-      } catch (e) {
-        if (e instanceof yup.ValidationError) {
-            success = false;
-            errorArray = e.errors;
-        }
+      await passengerSchema.validate(passenger, { abortEarly: false });
+    } catch (e) {
+      if (e instanceof yup.ValidationError) {
+        success = false;
+        errorArray = e.errors;
+      }
     }
     if (passenger.email !== passenger.confEmail) {
-        success = false;
-        errorArray.push("Emails do not match")
+      success = false;
+      errorArray.push('Emails do not match');
     }
 
     if (!success)
-    toast({
+      toast({
         title: 'Error!',
         description: generateErrorMsg(errorArray, index),
         status: 'error',
@@ -120,9 +119,9 @@ export const PassengerDetailsPage = ({
 
   const generateErrorMsg = (array: string[], index: number) => {
     let errorMsg = 'Incorrectly filled for Passenger ' + index + ':';
-    array.forEach(error => errorMsg = errorMsg + " - " + error);
+    array.forEach((error) => (errorMsg = errorMsg + ' - ' + error));
     return errorMsg;
-  }
+  };
 
   const handleCurrentUser = () => {
     if (user) {
@@ -215,119 +214,119 @@ export const PassengerDetailsPage = ({
     }
   };
 
-    const handleAddPassenger = () => {
-        firstNames.push('');
-        lastNames.push('');
-        emails.push('');
-        confEmails.push('');
-    }
+  const handleAddPassenger = () => {
+    firstNames.push('');
+    lastNames.push('');
+    emails.push('');
+    confEmails.push('');
+  };
 
-    const handleRemovePassenger = (index: number)  => {
-        firstNames.splice(index, 1);
-        lastNames.splice(index, 1);
-        emails.splice(index, 1);
-        confEmails.splice(index, 1);
-    }
+  const handleRemovePassenger = (index: number) => {
+    firstNames.splice(index, 1);
+    lastNames.splice(index, 1);
+    emails.splice(index, 1);
+    confEmails.splice(index, 1);
+  };
 
-    const renderPassengerForms = () => {
-        var forms = [];
-        for (var i = 0; i < passengerCount; i++) {
-            if (i === 0) {
-                forms.push(
-                    <VStack>
-                        <Text>Passenger {i + 1}</Text>
-                        <HStack w='full'>
-                            <FormControl flex={1}>
-                                <FormLabel>First Name</FormLabel>
-                                <Input value={firstNames[0]} name='fname' onChange={handleChange(0)}/>
-                            </FormControl>
-                            <FormControl flex={1}>
-                                <FormLabel>Last Name</FormLabel>
-                                <Input value={lastNames[0]} name='lname' onChange={handleChange(0)}/>
-                            </FormControl>
-                        </HStack>
-                        <HStack w='full'>
-                            <FormControl flex={1}>
-                                <FormLabel>Email Address</FormLabel>
-                                <Input value={emails[0]} name='email' onChange={handleChange(0)}/>
-                            </FormControl>
-                            <FormControl flex={1}>
-                                <FormLabel>Confirm Email Address</FormLabel>
-                                <Input value={confEmails[0]} name='confemail' onChange={handleChange(0)}/>
-                            </FormControl>
-                        </HStack>
-                        <FormControl>
-                            <FormLabel>Class</FormLabel>
-                            <Select>
-                                {ticketOptions.map((o) => 
-                                    <option value={o.key}>{o.label}</option>
-                                )}
-                            </Select>
-                        </FormControl>
-                        <Text
-                            as='u'
-                            colorScheme='gray'
-                            onClick={() => {
-                                handleCurrentUser();
-                            }}
-                            >
-                            {' '}
-                            Set as Me
-                        </Text>
-                    </VStack>
-                );
-            } else {
-                forms.push(
-                    <VStack>
-                        <Text>Passenger {i + 1}</Text>
-                        <HStack w='full'>
-                            <FormControl flex={1}>
-                                <FormLabel>First Name</FormLabel>
-                                <Input value={firstNames[i]} name={'fname'} onChange={handleChange(i)}/>
-                            </FormControl>
-                            <FormControl flex={1}>
-                                <FormLabel>Last Name</FormLabel>
-                                <Input value={lastNames[i]} name={'lname'} onChange={handleChange(i)}/>
-                            </FormControl>
-                        </HStack>
-                        <HStack w='full'>
-                            <FormControl flex={1}>
-                                <FormLabel>Email Address</FormLabel>
-                                <Input value={emails[i]} name={'email'} onChange={handleChange(i)}/>
-                            </FormControl>
-                            <FormControl flex={1}>
-                                <FormLabel>Confirm Email Address</FormLabel>
-                                <Input value={confEmails[i]} name={'confemail'} onChange={handleChange(i)}/>
-                            </FormControl>
-                        </HStack>
-                        <FormControl>
-                            <FormLabel>Class</FormLabel>
-                            <Select>
-                                {ticketOptions.map((o) => 
-                                    <option value={o.key}>{o.label}</option>
-                                )}
-                            </Select>
-                        </FormControl>
-                        <Button
-                            id={i.toString()}
-                            colorScheme='gray'
-                            name={i.toString()}
-                            onClick={(e) => {
-                                let index = Number(e.currentTarget.getAttribute("id"));
-                                console.log(index);
-                                handleRemovePassenger(index);
-                                setPassengerCount(passengerCount - 1);
-                            }}
-                            >
-                            {' '}
-                            Remove
-                        </Button>
-                    </VStack>
-                );
-            }
-        }
-        return forms;
+  const renderPassengerForms = () => {
+    var forms = [];
+    for (var i = 0; i < passengerCount; i++) {
+      if (i === 0) {
+        forms.push(
+          <VStack>
+            <Text>Passenger {i + 1}</Text>
+            <HStack w='full'>
+              <FormControl flex={1}>
+                <FormLabel>First Name</FormLabel>
+                <Input value={firstNames[0]} name='fname' onChange={handleChange(0)} />
+              </FormControl>
+              <FormControl flex={1}>
+                <FormLabel>Last Name</FormLabel>
+                <Input value={lastNames[0]} name='lname' onChange={handleChange(0)} />
+              </FormControl>
+            </HStack>
+            <HStack w='full'>
+              <FormControl flex={1}>
+                <FormLabel>Email Address</FormLabel>
+                <Input value={emails[0]} name='email' onChange={handleChange(0)} />
+              </FormControl>
+              <FormControl flex={1}>
+                <FormLabel>Confirm Email Address</FormLabel>
+                <Input value={confEmails[0]} name='confemail' onChange={handleChange(0)} />
+              </FormControl>
+            </HStack>
+            <FormControl>
+              <FormLabel>Class</FormLabel>
+              <Select>
+                {ticketOptions.map((o) => (
+                  <option value={o.key}>{o.label}</option>
+                ))}
+              </Select>
+            </FormControl>
+            <Text
+              as='u'
+              colorScheme='gray'
+              onClick={() => {
+                handleCurrentUser();
+              }}
+            >
+              {' '}
+              Set as Me
+            </Text>
+          </VStack>
+        );
+      } else {
+        forms.push(
+          <VStack>
+            <Text>Passenger {i + 1}</Text>
+            <HStack w='full'>
+              <FormControl flex={1}>
+                <FormLabel>First Name</FormLabel>
+                <Input value={firstNames[i]} name={'fname'} onChange={handleChange(i)} />
+              </FormControl>
+              <FormControl flex={1}>
+                <FormLabel>Last Name</FormLabel>
+                <Input value={lastNames[i]} name={'lname'} onChange={handleChange(i)} />
+              </FormControl>
+            </HStack>
+            <HStack w='full'>
+              <FormControl flex={1}>
+                <FormLabel>Email Address</FormLabel>
+                <Input value={emails[i]} name={'email'} onChange={handleChange(i)} />
+              </FormControl>
+              <FormControl flex={1}>
+                <FormLabel>Confirm Email Address</FormLabel>
+                <Input value={confEmails[i]} name={'confemail'} onChange={handleChange(i)} />
+              </FormControl>
+            </HStack>
+            <FormControl>
+              <FormLabel>Class</FormLabel>
+              <Select>
+                {ticketOptions.map((o) => (
+                  <option value={o.key}>{o.label}</option>
+                ))}
+              </Select>
+            </FormControl>
+            <Button
+              id={i.toString()}
+              colorScheme='gray'
+              name={i.toString()}
+              onClick={(e) => {
+                let index = Number(e.currentTarget.getAttribute('id'));
+                console.log(index);
+                handleRemovePassenger(index);
+                setPassengerCount(passengerCount - 1);
+              }}
+            >
+              {' '}
+              Remove
+            </Button>
+          </VStack>
+        );
+      }
     }
+    return forms;
+  };
 
   const renderFlightDetails = () => {
     return (
@@ -391,26 +390,30 @@ export const PassengerDetailsPage = ({
     );
   };
 
-    if (!user) {
-        navigate(routes.login, {state: {redirectUrl: routes.home}})
-    }
+  if (!user) {
+    navigate(routes.login, { state: { redirectUrl: routes.home } });
+  }
 
   return (
     <Flex justifyContent='center' p='5em'>
-        <Flex justifyContent='center' direction='column' w='50em'>
-            {renderFlightDetails()}
-            {renderPassengerForms()}
-            <Button leftIcon={<BsFillPlusCircleFill/>} 
-                onClick={() => {
-                    handleAddPassenger();
-                    setPassengerCount(passengerCount + 1);
-                }}>Add another passenger</Button>
-            <Button
-            type='button'
-            colorScheme='red'
-            onClick={async () => {
-                if(await submitEvent()){
-                    let passengers: Passenger[] = [];
+      <Flex justifyContent='center' direction='column' w='50em'>
+        {renderFlightDetails()}
+        {renderPassengerForms()}
+        <Button
+          leftIcon={<BsFillPlusCircleFill />}
+          onClick={() => {
+            handleAddPassenger();
+            setPassengerCount(passengerCount + 1);
+          }}
+        >
+          Add another passenger
+        </Button>
+        <Button
+          type='button'
+          colorScheme='red'
+          onClick={async () => {
+            if (await submitEvent()) {
+              let passengers: Passenger[] = [];
 
               for (var i = 0; i < passengerCount; i++) {
                 passengers.push({
